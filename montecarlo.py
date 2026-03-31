621e0e926ce260db7101bd95542b29ee810d3ead
Andre Hirosaki <andre.hirosaki@icomp.ufam.edu.br>
2026-03-31 11:57:03 -0400

    new clinical results

--- montecarlo.py
+++ montecarlo.py
@@ -14,18 +14,21 @@ def random_dataset(denguefile, undeffile, chikfile, s):
     df = []
 
     #skip = sorted(random.sample(range(1, 4307513+1), 4307513-s))
-    #df.append(pd.read_csv(denguefile, skiprows=skip, dtype=str))
+    #df.append(pd.read_csv(denguefile, skiprows=skip))
 
     skip = sorted(random.sample(range(1, 2100029+1), 2100029-s))
-    df.append(pd.read_csv(undeffile, skiprows=skip, dtype=str))
+    df.append(pd.read_csv(undeffile, skiprows=skip))
 
-    df.append(pd.read_csv(chikfile, dtype=str))
+    df.append(pd.read_csv(chikfile))
 
     return pd.concat(df)
 
 def process_dataset(df):
-    cols = ['ID_AGRAVO', "DT_NOTIFIC", "SEM_NOT", "NU_ANO", "SG_UF_NOT", "ID_MUNICIP", "ID_REGIONA", "ID_UNIDADE", "DT_SIN_PRI", "SEM_PRI", "SG_UF",
-            "ID_MN_RESI", "ID_RG_RESI", "VOMITO", "LACO", "ID_PAIS", "DT_INVEST", "TPAUTOCTO", "COUFINF", "COPAISINF", "COMUNINF", "DT_ENCERRA", "DT_NASC",
+    cols = ['ID_AGRAVO', "DT_NOTIFIC", "SEM_NOT", "NU_ANO", "SG_UF_NOT", "ID_MUNICIP", "ID_REGIONA", "ID_UNIDADE", 
+            "DT_SIN_PRI", "SEM_PRI", "SG_UF", 'NU_IDADE_N', 'CS_SEXO', 'CS_GESTANT', 'CS_RACA', 'CS_ESCOL_N',
+            "ID_MN_RESI", "ID_RG_RESI", "VOMITO", "LACO", "ID_PAIS", "DT_INVEST", "TPAUTOCTO", "COUFINF", 
+            "COPAISINF", "COMUNINF", "DT_ENCERRA", "DT_NASC", 'HEMATOLOG', 'HEPATOPAT', 'RENAL', 'ACIDO_PEPT',
+            'AUTO_IMUNE', 'HOSPITALIZ', 'EVOLUCAO',
             "RESUL_SORO", "RESUL_NS1", "RESUL_VI_N", "RESUL_PCR_", "HISTOPA_N", "IMUNOH_N", "LEUCOPENIA", 'CLASSI_FIN'] # removido vomito e laco
 
     x = df.drop(cols, axis=1)
@@ -48,7 +51,8 @@ def process_dataset(df):
     return x_train, x_test, y_train, y_test
 
 def run_classifier(classifier):
-    results_file = open(f"{classifier.__class__.__name__}chikundef_perclass.csv", "a")
+    results_file = open(f"{classifier.__class__.__name__}chikundefclinical.csv", "a")
+    results_file2 = open(f"{classifier.__class__.__name__}chikundefclinical_perclass.csv", "a")
     #results_file.write("num, accuracy, precision, recall, f1")
 
     sample = 325_000 # undersample para classe chikungunya
@@ -59,41 +63,41 @@ def run_classifier(classifier):
 
     classifier.fit(x_train, y_train)
     y_pred = classifier.predict(x_test)
-    
-    
-    """
+
     acc = accuracy_score(y_test, y_pred)
     prec = precision_score(y_test, y_pred, average='macro')
     rec = recall_score(y_test, y_pred, average='macro')
     f1 = f1_score(y_test, y_pred, average='macro')
     
     results_file.write(f"{acc}, {prec}, {rec}, {f1}\n")
-    """
-    #labels = ['dengue', 'discarded/inconclusive']
+    
+    #labels = ['chikung', dengue', 'discarded/inconclusive']
     labels = [0, 1]
     prec, rec, f1, support = precision_recall_fscore_support(y_test, y_pred, labels=labels, average=None)
     
     chik_prec = prec[0]
     chik_rec = rec[0]
     chik_f1 = f1[0]
-    """
-    deng_prec = prec[0]
-    deng_rec = rec[0]
-    deng_f1 = f1[0]
-    """
-    undef_prec = prec[1]
-    undef_rec = rec[1]
-    undef_f1 = f1[1]
     
+    deng_prec = prec[1]
+    deng_rec = rec[1]
+    deng_f1 = f1[1]
+    """
+    undef_prec = prec[2]
+    undef_rec = rec[2]
+    undef_f1 = f1[2]
+    """
     # chik -> dengue -> undef
     # precisao -> recall -> f1
-    results_file.write(f"{chik_prec}, {chik_rec}, {chik_f1}, {undef_prec}, {undef_rec}, {undef_f1}\n")
+    results_file2.write(f"{chik_prec}, {chik_rec}, {chik_f1}, {deng_prec}, {deng_rec}, {deng_f1}\n")
     
+    results_file.close()
+    results_file2.close()
 
 def main():
-    from sklearn.neural_network import MLPClassifier
+    from sklearn.naive_bayes import GaussianNB
 
-    clf = MLPClassifier(hidden_layer_sizes=(100,), learning_rate_init=0.1)
+    clf = GaussianNB()
     
     run_classifier(clf)
 
